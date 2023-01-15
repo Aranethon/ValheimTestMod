7d5ec48761df2a14c51298f8338a9a452f595761
Aranethon <ryanewing25@hotmail.com>
2023-01-14 18:12:07 -0800

    Add project files.

---
+++ ValheimTestMod/ATestMod.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ATestMod
+{
+    [BepInPlugin(PluginID, "ATestMod", "1.0.1")]
+    public class ATestMod : BaseUnityPlugin
+    {
+        private static ATestMod _instance;
+        private static ConfigEntry<bool> _loggingEnabled;
+
+        public const string PluginID = "Aranethon.mods.ATestMod";
+
+        private Harmony _harmony;
+
+        public static void Log(string message)
+        {
+            if (_loggingEnabled.Value)
+                _instance.Logger.LogInfo(message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            if (_loggingEnabled.Value)
+                _instance.Logger.LogWarning(message);
+        }
+
+        public static void LogError(string message)
+        {
+            if (_loggingEnabled.Value)
+                _instance.Logger.LogError(message);
+        }
+
+        private void Awake()
+        {
+            _loggingEnabled = Config.Bind("Logging", "Logging Enabled", true, "Enable logging");
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginID);
+            _instance = this;
+        }
+
+        [HarmonyPatch(typeof(InventoryGui), nameof(InventoryGui.Awake))]
+        public static class InventoryGui_Awake_Patch
+        {
+            [HarmonyPriority(Priority.Low)]
+            public static void PostFix(InventoryGui __instance)
+            {
+                __instance.m_containerName.color = Color.green;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+            _instance = null;
+        }
+    }
+}
